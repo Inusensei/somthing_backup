839f97ef43a161153e63098381d6429f630efa20
inusensei <inusensei@163.com>
2023-05-25 16:25:17 +0800

    add icon and dashboard

--- init.lua
+++ init.lua
@@ -54,6 +54,47 @@ require("lazy").setup({
 		lazy = true,
 	},
 	{
+		'glepnir/dashboard-nvim',
+		event = 'VimEnter',
+		config = function()
+			require('dashboard').setup {
+				-- config
+				theme = 'hyper',
+				config = {
+					week_header = {
+						enable = true,
+					},
+					shortcut = {
+						{ desc = '󰊳 Update', group = '@property', action = 'Lazy update',
+							key = 'u' },
+						{
+							icon = ' ',
+							icon_hl = '@variable',
+							desc = 'Files',
+							group = 'Label',
+							action = 'Telescope find_files',
+							key = 'f',
+						},
+						{
+							desc = ' Apps',
+							group = 'DiagnosticHint',
+							action = 'Telescope app',
+							key = 'a',
+						},
+						{
+							desc = ' dotfiles',
+							group = 'Number',
+							action = 'Telescope dotfiles',
+							key = 'd',
+						},
+					},
+					footer = {"Bright star,would I were steadfast as thou art"},
+				},
+			}
+		end,
+	--	dependencies = {  'nvim-tree/nvim-web-devicons'  }
+	},
+	{
 		"nvim-lualine/lualine.nvim",
 		config = function()
 			require("lualine").setup({})
@@ -76,6 +117,9 @@ require("lazy").setup({
 			]])
 		end,
 		"preservim/nerdtree",
+		dependencies = {
+			"ryanoasis/vim-devicons",
+		},
 	},
 	{
 		event = "VeryLazy",
